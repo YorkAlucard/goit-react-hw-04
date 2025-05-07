2aa4793f31ef4ccc4a205beb8db23de958cf4cc2
York Alucard <york.alucard@gmail.com>
2025-05-07 17:30:34 +0300

    fix src/components

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Modal from 'react-modal';
 import './index.css';
-import App from './components/App/App';
+import App from './сomponents/App/App';
 
 Modal.setAppElement('#root');
 
